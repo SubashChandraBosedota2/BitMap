5a8dbc892892d8c8f4bb13fcac5bf00cb2be0e75
SubashChandraBosedota2 <36149589+SubashChandraBosedota2@users.noreply.github.com>
2019-03-16 00:14:10 +0530

    Update Bitmap.java

--- Bitmap.java
+++ Bitmap.java
@@ -1,6 +1,6 @@
 /**
  * Bitmap editor is a simple program which use to genrate bitmap and to edit
- * IN this program we can able to create an pixel by 2D array 
+ * IN this program we  can  create  the pixel by 2D array 
  *@author  :subash
  *@version :1.0
  *@created :4-08-2017 
